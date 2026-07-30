d13adb394bd448dfb1e04eaea71c62e81bdcf645
Gert010 <iskander.gimranov05@gmail.com>
2026-07-31 00:54:06 +0500

    Update join.js

--- js/join.js
+++ js/join.js
@@ -1,1 +1,101 @@
+import { db } from "./firebase.js";
 
+import {
+    collection,
+    getDocs,
+    addDoc,
+    query,
+    where,
+    doc,
+    getDoc,
+    updateDoc
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+
+const membersCollection = collection(db, "members");
+
+async function loadCounter() {
+
+    const statsRef = doc(db, "members", "stats");
+    const statsSnap = await getDoc(statsRef);
+
+    if (statsSnap.exists()) {
+        document.getElementById("count").textContent =
+            statsSnap.data().count;
+    }
+
+}
+
+window.addEventListener("load", loadCounter);
+
+window.join = async function(event){
+
+    event.preventDefault();
+
+    const nickname =
+        document.querySelectorAll("input")[0].value.trim();
+
+    const email =
+        document.querySelectorAll("input")[1].value.trim().toLowerCase();
+
+    const q = query(
+        membersCollection,
+        where("email","==",email)
+    );
+
+    const exists = await getDocs(q);
+
+    if(!exists.empty){
+
+        document.getElementById("msg").textContent =
+        "Эта почта уже зарегистрирована.";
+
+        return;
+
+    }
+  
+  const statsRef = doc(db, "members", "stats");
+    const statsSnap = await getDoc(statsRef);
+
+    let count = 17;
+
+    if (statsSnap.exists()) {
+        count = statsSnap.data().count;
+    }
+
+    count++;
+
+    const gertId = "GERT-" + String(count).padStart(4, "0");
+
+    await addDoc(membersCollection,{
+        nickname: nickname,
+        email: email,
+        id: gertId,
+        joined: new Date().toISOString()
+    });
+
+    await updateDoc(statsRef,{
+        count: count
+    });
+
+    document.getElementById("count").textContent = count;
+
+    document.getElementById("msg").textContent =
+        "Добро пожаловать в GERT!";
+
+    alert(
+`══════════════════════
+
+Добро пожаловать в GERT!
+
+Позывной: ${nickname}
+
+Ваш идентификатор:
+
+${gertId}
+
+══════════════════════`
+    );
+
+    event.target.reset();
+
+}
